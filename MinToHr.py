331fc41af8d8c821da6321acc63869e5b8b2a332
yuvarajkarunakaran <44950024+yuvarajkarunakaran@users.noreply.github.com>
2019-06-30 18:14:51 +0530

    Update MinToHr.py

--- MinToHr.py
+++ MinToHr.py
@@ -2,10 +2,10 @@ n=int(input())
 hr=0
 min=0
 if n<60:
-    print(hr,end='')
+    print(hr,end=' ')
     print(min+n)
 else:
     a=n//60
-    print(a,end='')
+    print(a,end=' ')
     b=n%60
-    print(b,end='')
+    print(b)
