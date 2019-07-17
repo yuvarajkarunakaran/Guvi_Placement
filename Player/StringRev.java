b70ed537fd083db8e911241dd63ffe101d087c8f
Yuvaraj M K <44950024+yuvarajkarunakaran@users.noreply.github.com>
2019-07-18 00:05:25 +0530

    Update StringRev.java

--- Player/StringRev.java
+++ Player/StringRev.java
@@ -4,7 +4,6 @@ public class StringRev {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        String n=sc.nextLine();
         System.out.print(n.length());
         for(int i=n.length()-1;i>=0;i--)
         {
